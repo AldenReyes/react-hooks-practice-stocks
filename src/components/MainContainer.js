46f882086b07e8ab2d3aaa2c3bb2c63e14cf75d4
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-06-25 17:15:47 -0700

    Adds functionality: renders stocks on page from db.json, as well as initial stock click functionality

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [portfolioStocks, setPortfolioStocks] = useState([]);
+  const [stocks, setStocks] = useState(null);
+
+  const fetchStocksData = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/stocks");
+      const data = await response.json();
+      setStocks(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStocksData();
+  }, []);
+
+  const handleAddStock = () => {
+    console.log("click");
+  };
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer onHandleAddStock={handleAddStock} stocks={stocks} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolioStocks={portfolioStocks} />
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ onHandleAddStock, stocks }) {
+  if (stocks === null) {
+    return (
+      <div>
+        <h2>Stocks</h2>
+        <Stock
+          key="Loading..."
+          ticker="Loading..."
+          name="Loading..."
+          price="Loading..."
+        />
+      </div>
+    );
+  }
+
+  const renderedStocks = stocks.map((stock) => {
+    return (
+      <Stock
+        key={stock.id}
+        ticker={stock.ticker}
+        name={stock.name}
+        price={stock.price}
+        onHandleStock={onHandleAddStock}
+      />
+    );
+  });
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {renderedStocks}
     </div>
   );
 }
